c8e72d60cd5af227b9a7349e2f85775ce800df59
davidckim <davidckim320@gmail.com>
2015-02-25 17:28:27 -0800

    added resizing.

--- js/index.js
+++ js/index.js
@@ -1,36 +1,4 @@
 $(document).ready(function() {
-  bindEvents();
 
 });
 
-
-function bindEvents() {
-  bindAbout();
-  bindPortfolio();
-  bindContact();
-  bindBlog();
-}
-
-function bindAbout () {
-  $('#about').click(function(e) {
-  	e.preventDefault();
-  });
-}
-
-function bindPortfolio () {
-  $('#portfolio').click(function(e) {
-  	e.preventDefault();
-  });
-}
-
-function bindContact () {
-  $('#contact').click(function(e) {
-  	e.preventDefault();
-  });
-}
-
-function bindBlog () {
-  $('#blog').click(function(e) {
-  	e.preventDefault();
-  });
-}
