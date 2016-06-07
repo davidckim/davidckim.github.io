cf3328bfef6382add75cccf0a6c309a02317150b
davidckim <davidckim320@gmail.com>
2016-06-07 00:00:32 -0700

    second.

--- js/main.js
+++ js/main.js
@@ -9,13 +9,29 @@ document.addEventListener("DOMContentLoaded", function() {
   	for ( var i = 0; i < 3; i++ ) {
   		(function(i) {
 		  	tab[i].addEventListener('click', function(event) {
+		  		for ( var n = 0; n < 3; n++ ) {
+		  			tab[n].classList.remove('active');
+		  		}
 		  		event.preventDefault();
 		  		tab[i].className += " active";
-		  		console.log(tab[i].className);
+		  		changeContent(tab[i].getAttribute('id'));
 		  	});
   		})(i);
   	}
   }
+
+ 	function changeContent(tab) {
+ 		var content = document.getElementsByTagName('p');
+ 		if ( tab === 'about' ) {
+ 			console.log('about tab');
+ 		} else if ( tab === 'projects' ) {
+ 			console.log('projects tab');
+ 		} else if ( tab === 'contact' ) {
+ 			console.log('contact tab');
+ 		} else {
+ 			console.log('uh...')
+ 		}
+ 	}
 
   init();
 
